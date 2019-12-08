0accb0b34258eb848b3ba16183395db2fcbd7dc4
enorkus <enorkus@enorkus.com>
2019-12-08 17:58:50 +0200

    Script for extracting part of train files to test directory

---
+++ move_files_to_test.py
@@ -0,0 +1,29 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import shutil
+
+TRAIN_DIR = "train/"
+TEST_DIR = "test/"
+
+allTrainFiles = [f for f in listdir(TRAIN_DIR) if isfile(join(TRAIN_DIR, f))]
+print("All train files:")
+print(len(allTrainFiles))
+
+count = 0
+numberOfTestFiles = len(allTrainFiles) * 0.1
+os.mkdir(TEST_DIR)
+for fileName in allTrainFiles:
+    if allTrainFiles.index(fileName) < numberOfTestFiles:
+        shutil.move(TRAIN_DIR + fileName, TEST_DIR + fileName)
+        count += 1
+    else:
+        break
+
+trainFiles = [f for f in listdir(TRAIN_DIR) if isfile(join(TRAIN_DIR, f))]
+print("Train files:")
+print(len(trainFiles))
+
+testFiles = [f for f in listdir(TEST_DIR) if isfile(join(TEST_DIR, f))]
+print("Test files:")
+print(len(testFiles))
